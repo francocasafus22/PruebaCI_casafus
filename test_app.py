8da033a082c4f281e115bc778d78e7dd49dd1072
francocasafus22 <francocasafus55@gmail.com>
2025-10-16 16:33:27 -0300

    add test

--- test_app.py
+++ test_app.py
@@ -1,7 +1,10 @@
-from app import suma, resta
+from app import suma, resta, division
 
 def test_suma () :
     assert suma(2,3) == 5
 
 def test_resta():
     assert resta(3,2) == 1
+
+def test_division():
+    assert division(5,2)==2.5
